109e106d9ecc781d212abd1dfa33369334f34336
JINGLUO <leojing1989@gmail.com>
2018-05-13 21:22:06 +1000

    - [x] 充电状态下app在前台保持屏幕常亮

--- Timer-Weather/Timer-Weather/AppDelegate.swift
+++ Timer-Weather/Timer-Weather/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-//        SettingsViewModel.sharedInstance.resetDefaults()
+        if UIDevice.current.batteryState == .charging {
+            application.isIdleTimerDisabled = true
+        }
         
         if let _ = Preferences.sharedInstance.getShowDate() {
         } else {
